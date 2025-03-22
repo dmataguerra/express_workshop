e99deb88917b6305bb705126b7fe300e214ecf0a
David <dmataguerra@outlook.com>
2025-03-21 22:26:21 -0600

    Added nodemon and GET verbs

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const app = express(); //lamar el constructor de express
 app.get("/", (req, res, next) => {
   res.status(200); //Codigo 200 es el codigo de respuesta de una peticion 
   res.send("Bienvenido");
-})
+});
 
-app.listen(3000, () => {
+app.get("/:name", (req, res, next) => {
+  console.log(req.params.name);
+  res.status(200);
+  res.send("Estas en la pagina Nombre");
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running...');
 });
